2f9d214e029933e7fab085b05d941bcba8e3ed6a
buuninbee <buunin.bee@gmail.com>
2026-01-05 00:00:46 -0300

    feat: configuração inicial das rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,25 @@
 import { StrictMode } from "react";
 import "./index.css";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router";
 import App from "./App.jsx";
 import DashboardKrs from "./pages/DashboardKrs.jsx";
+
+import { AppSidebar } from "@/components/app-sedebar";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="/dashboard-krs" element={<DashboardKrs />} />
-      </Routes>
+      <SidebarProvider>
+        <AppSidebar />
+
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="/dashboard-krs" element={<DashboardKrs />} />
+        </Routes>
+      </SidebarProvider>
     </BrowserRouter>
   </StrictMode>
 );
